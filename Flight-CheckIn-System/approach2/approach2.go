86d23ab6a34a85c780695c3bceba2394e0c37f69
faizmohammad <faizmohammad@microsft.com>
2025-09-07 07:55:01 +0000

    add approach where seat number is randomly selected

--- Flight-CheckIn-System/approach2/approach2.go
+++ Flight-CheckIn-System/approach2/approach2.go
@@ -5,96 +5,143 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
-	"sync"
+	"math/rand"
 )
 
-type User struct {
-	Name  string
-	Id    int64
-}
+var db *sql.DB
 
-type Seat struct {
-	Number int
-	Row    int
-}
-
-func main() {
-	//  book 200 seats for 200 users concurrently by spinning 200 goroutines
-	//  each goroutine should book a seat for a user and print the user and seat
-
-	//  we will have a db connection and update the seat as booked for the user
-
-	//  these are the values used while creating posgres container locally
-	connStr1 := "user=postgres dbname=postgres port=5432 user=postgres password=mysecretpassword sslmode=disable"
-	db, err := sql.Open("postgres", connStr1)	
+func initDB() {
+	// Initialize database connection here
+	connStr := "user=postgres dbname=postgres port=5432 user=postgres password=mysecretpassword sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}else {
+		// Actually try to connect
+		err = db.Ping()
+		if err != nil {
+			log.Fatal("Database is unreachable: ", err)
+		}
+
 		log.Println("Database connection established successfully")
 	}
+}
 
-	users := make([]User, 200)
-	seats := make([]Seat, 200)
-	for i := 0; i < 200; i++ {
-		users[i] = User{
-			Name:  "User" + string(i),
-			Id:    int64(i),
-		}
-
-		seats[i] = Seat{
-			Number: i + 1,
-			Row:    (i / 10) + 1,
-		}
-	}
-
-	var wg sync.WaitGroup
-	for i := 0; i < 200; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			user := users[i]
-			// println("Booked seat", seat.Number, "in row", seat.Row, "for user", user.Name, "with id", user.Id)
-			//  goroutines should book a seat for a user in the database
-			//  update the seat as booked for the user
-			//  find a seat which is not booked and book it for the user
-			db.Exec("UPDATE flight_Seats SET user_id = $1 WHERE seat_id = (SELECT seat_id FROM flight_Seats WHERE user_id IS NULL LIMIT 1 FOR UPDATE)", user.Id)
-		}(i)
-	}
-
-	wg.Wait()
-
-	//  print all the seats booked for users. Show output by print rows and x which is booked
-	rows, err := db.Query("SELECT * FROM flight_Seats ORDER BY seat_id")
+func resetDB() {
+	//  reset data in users and seats table.
+	_, err := db.Exec("UPDATE seats SET user_id = NULL")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to reset database: ", err)
 	}
+}
 
-	var seatStatus [200]string
+func printSeats() {
+	fmt.Println("Current seats layout (5x4):")
 
+	// query all seats ordered by seat_id
+	rows, err := db.Query("SELECT seat_id, user_id FROM seats ORDER BY seat_id")
+	if err != nil {
+		log.Fatal("Failed to query seats: ", err)
+	}
 	defer rows.Close()
+
+	seats := make([]int, 0, 20) // store user_id; 0 means empty
 	for rows.Next() {
-		var userId sql.NullInt64
-		
-		//  fetch seat number from flight_Seats table
-		//  print user id and seat number
-		var seat_id int
-		if err := rows.Scan(&seat_id, &userId); err != nil {
+		var seatID, userID sql.NullInt32
+		err := rows.Scan(&seatID, &userID)
+		if err != nil {
 			log.Fatal(err)
 		}
-	
-		if userId.Valid {
-			log.Printf("User with id %d has booked seat %d", userId.Int64, seat_id)
-			seatStatus[seat_id-1] = "X"
+
+		if userID.Valid {
+			seats = append(seats, int(userID.Int32))
 		} else {
-			seatStatus[seat_id-1] = "O"
+			seats = append(seats, 0)
 		}
 	}
 
-	seatsPerRow := 10
-	for i := 0; i < 200; i++ {
-		fmt.Print(seatStatus[i], " ")
-		if (i + 1) % seatsPerRow == 0 {
-			fmt.Println() // new row
+	// print as 5x5 grid
+	for i := 0; i < 20; i++ {
+		if seats[i] == 0 {
+			fmt.Print(". ")
+		} else {
+			fmt.Print("x ")
+		}
+
+		if (i+1)%5 == 0 {
+			fmt.Println()
 		}
 	}
+}
+
+
+func main() {
+	initDB()
+	// resetDB()
+	fmt.Println("Enter  user id for whom you want to book a seat:")
+
+	var userId int
+	fmt.Scanln(&userId)
+
+	bookSeat(userId)
+	printSeats()
+}
+
+func bookSeat(userId int) {
+	fmt.Printf("Booking seat for user %d\n", userId)
+
+	// begin transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal("Failed to begin transaction: ", err)
+		return
+	}
+
+	//  get all seats and then select random seat from that list
+	allSeats := fetchAllSeats()
+	seatNumber := selectRandomSeat(allSeats)
+
+	// book the seat
+	_, err = tx.Exec("UPDATE seats SET user_id = $1 WHERE seat_id = $2", userId, seatNumber)
+	if err != nil {
+		tx.Rollback()
+		log.Fatal("Failed to book seat: ", err)
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("Failed to commit transaction: ", err)
+		return
+	}
+}
+
+
+func fetchAllSeats() []int {
+	seats := make([]int, 0)
+	rows, err := db.Query("SELECT seat_id FROM seats WHERE user_id IS NULL")
+	if err != nil {
+		log.Fatal("Failed to fetch seats: ", err)
+		return seats
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var seatID int
+		if err := rows.Scan(&seatID); err != nil {
+			log.Fatal("Failed to scan seat: ", err)
+		}
+		seats = append(seats, seatID)
+	}
+	return seats
+}
+
+func selectRandomSeat(allSeats []int) int {
+	if len(allSeats) == 0 {
+		log.Fatal("No available seats found")
+	}
+
+	// Select a random seat from the available seats
+	return allSeats[rand.Intn(len(allSeats))]
 }
